16b8267629ed566853f8ec59f86052814cc5b99e
Isac Junior <isac_asj91@hotmail.com>
2020-09-23 00:53:33 -0300

    create doc_widget_annotation package

---
+++ packages/doc_widget_annotation/lib/doc_widget_annotation.dart
@@ -0,0 +1,17 @@
+library doc_widget_annotation;
+
+/// Annotation for the `doc_widget` package.
+class DocWidget {
+  const DocWidget._();
+}
+
+/// Annotation for the `doc_widget` package.
+///
+/// This annotation is used to generate your documentation
+///
+/// ```dart
+/// @docWidget
+/// class MyWidget extends StatelessWidget {
+/// }
+/// ```
+const docWidget = DocWidget._();
